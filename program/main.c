19aba63328fdddef1dbbb85cc769c7e858a6f0e6
wphkweerasinghe <hkalhara777@gmail.com>
2021-06-15 15:25:25 +0530

    Access systhem time and calculate square value

--- program/main.c
+++ program/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <time.h>
 
 int main()
 {
@@ -9,4 +10,19 @@ int main()
     //Print Mac address
     printf("*Mac Address");
     system("getmac");
+
+    //Display Square value
+    int number; //declare variable call 'number'
+    int square; //declare variable call 'square'
+    time_t rawtime;
+    struct td*timeinfo;
+
+    printf("Enter a number:-");
+    scanf("%d",&number); //get user input to the number variable
+    square=number*number; //calculate square
+
+    time(&rawtime);
+    timeinfo=localtime(&rawtime);
+    printf("Square of %d is:- %d System time:%s ",number,square,asctime(timeinfo));//Display answer
+    return 0;
 }
